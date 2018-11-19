3ddb74396bc65999778cd463b439f876ac82ae9f
baiyuze <1347417308@qq.com>
2018-11-19 20:21:47 +0800

    Update router.js
    添加router

--- app/router.js
+++ app/router.js
@@ -4,4 +4,6 @@ module.exports = app => {
   app.get('/about', app.controller.home.index);
   app.get('/client', app.controller.home.client);
   app.get('/api/article/list', app.controller.home.pager);
+    //mock接口
+  app.all('/commodity/list',app.controller.mock.list);
 };
